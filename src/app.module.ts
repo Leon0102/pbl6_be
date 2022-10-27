f214084265d0686cb264461773355759eb382084
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-10-27 15:34:57 +0700

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { PropertiesModule } from './modules/properties/properties.module';
+import { FacilitiesModule } from './modules/facilities/facilities.module';
 @Module({
-  imports: [],
+  imports: [PropertiesModule, FacilitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { HttpExceptionFilter } from '@common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
