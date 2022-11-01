414459cc226b962788cd5c18e94860a507b7f069
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-11-01 21:43:00 +0700

    feat: added images

--- src/modules/properties/dtos/create-property.dto.ts
+++ src/modules/properties/dtos/create-property.dto.ts
@@ -13,11 +13,14 @@ import { PropertyDto } from './property.dto';
 import { CreateRoomTypeDto } from '@modules/room-types/dtos/create-room-type.dto';
 import { Type } from 'class-transformer';
 import { ValidateNested } from 'class-validator';
-import { RoomTypeDto } from '@modules/room-types/dtos/room-type.dto';
 export class CreatePropertyDto extends PropertyDto {
   @IsArray()
   @IsNotEmpty()
   @ValidateNested({ each: true })
-  @Type(() => RoomTypeDto)
-  roomTypes: RoomTypeDto[];
+  @Type(() => CreateRoomTypeDto)
+  roomTypes: CreateRoomTypeDto[];
+
+  @IsArray()
+  @IsNotEmpty()
+  images: string[];
 }
--- src/modules/room-types/dtos/create-room-type.dto.ts
+++ src/modules/room-types/dtos/create-room-type.dto.ts
@@ -1,5 +1,16 @@
-import { IsString, IsNotEmpty, IsNumber, Min, IsObject } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  Min,
+  IsObject,
+  IsArray,
+} from 'class-validator';
 import { RoomSize, Facility } from '../interfaces';
 import { RoomTypeDto } from './room-type.dto';
 
-export class CreateRoomTypeDto extends RoomTypeDto {}
+export class CreateRoomTypeDto extends RoomTypeDto {
+  @IsArray()
+  @IsNotEmpty()
+  images: string[];
+}
